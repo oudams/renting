2a89f82af56f47a1ea311789586de03a91b091db
Meas Outdam <mstdam@gmail.com>
2018-07-29 16:50:52 +0700

    feature impelement sign up, login, sing out - with authenticated_user? helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def authenticated_user?
+		user_signed_in?
+	end
 end
--- spec/system/pages_spec.rb
+++ spec/system/pages_spec.rb
@@ -5,4 +5,28 @@ describe "Pages" do
     visit root_path
     expect(page).to have_content("Welcome to Renting")
   end
+
+  it "can visit sign up page" do
+    visit root_path
+    click_link("Sign up")
+
+    expect(current_path).to eq(new_user_registration_path)
+  end
+
+  it "can visit login page" do
+    visit root_path
+
+    click_link("Login")
+    expect(current_path).to eq(new_user_session_path)
+  end
+
+  it "can logout" do
+    user = create(:user)
+
+    sign_in(user)
+    visit root_path
+    click_link("Sign out")
+
+    expect(page).to have_link("Login", href: new_user_session_path)
+  end
 end
