38427d7d9c6f8a8ddad1c788241e80f4343c0351
Meas Outdam <mstdam@gmail.com>
2018-07-29 12:13:59 +0700

    update support/capybara.rb

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,34 +1,10 @@
 require "selenium/webdriver"
 
-Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.register_driver :selenium_chrome_headless do |app|
-  browser_options = Selenium::WebDriver::Chrome::Options.new
-  browser_options.add_argument("--headless")
-  browser_options.add_argument("--disable-gpu")
-  browser_options.add_argument("--no-sandbox")
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
-end
-
-Capybara.javascript_driver = :selenium_chrome_headless
+# Silent puma server http://bit.ly/2HNiv4R
+Capybara.server = :puma, { Silent: true }
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :rack_test
-
-    # Silent puma server http://bit.ly/2HNiv4R
-    Capybara.server = :puma, { Silent: true }
-  end
-
-  config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome_headless
-  end
-
-  config.before(:each, type: :system, selenium_chrome: true) do
-    driven_by :selenium, using: :chrome
+    driven_by :rack_test # set to :selenium(slower and witout chrome headless) or :rack_test(faster and with chrome headless)
   end
 end
-
